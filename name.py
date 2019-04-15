590dbab9a4a3f5b5f93aecf4aa0bbb38794ce0b9
krishna26kulkarni <35299206+krishna26kulkarni@users.noreply.github.com>
2019-04-15 22:47:33 +0530

    final

--- name.py
+++ name.py
@@ -3,3 +3,4 @@ print("Welcome again to jenkins build automation")
 name="Krishna"
 print("My name is: " + name)
 print("Thanks for being part of this repo " + name)
+print("Nowm, this is final commit for today, go to sleep !!")
