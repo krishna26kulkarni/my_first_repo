f7b78c650e48cee8b569ad4a4244d15d04525620
krishna26kulkarni <35299206+krishna26kulkarni@users.noreply.github.com>
2019-04-15 21:11:32 +0530

    update

--- name.py
+++ name.py
@@ -2,4 +2,4 @@ print("Hello there from Python!!")
 print("Welcome again to jenkins build automation")
 name="Krishna"
 print("My name is: " + name)
-  
+print("Thanks for being part of this repo " + name)
